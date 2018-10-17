429d7587032540ca1d70c4b038c402d971858ad7
Farago Bence <semmiertelme13@gmail.com>
2018-10-17 14:27:01 +0200

    initial commit

---
+++ src/test/java/com/fb/coderlife/CoderlifeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fb.coderlife;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoderlifeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
